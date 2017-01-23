2332a90d300e2290ac6bf1b14ae34847a1b78082
yomaswn <yaumashf@gmail.com>
2017-01-23 11:08:40 +0700

    done

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // if(req.query && req.query.inputtt){
+    var kata = req.query.inputtt.toLowerCase()
+    res.render('form', {title:"judul",apa:"kata apa",answer:kata});
+  // }
 });
 
 module.exports = router;
